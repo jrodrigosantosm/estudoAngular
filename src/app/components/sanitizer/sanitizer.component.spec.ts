79ff7044d60937a5d42f12a89c20d7cf013a081c
Rodrigo Santos <rodrigom376@gmail.com>
2024-01-18 21:51:22 -0300

    components

---
+++ src/app/components/sanitizer/sanitizer.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { SanitizerComponent } from './sanitizer.component';
+
+describe('SanitizerComponent', () => {
+  let component: SanitizerComponent;
+  let fixture: ComponentFixture<SanitizerComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [SanitizerComponent]
+    })
+    .compileComponents();
+    
+    fixture = TestBed.createComponent(SanitizerComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/components/sanitizer/sanitizer.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-sanitizer',
+  standalone: true,
+  imports: [],
+  templateUrl: './sanitizer.component.html',
+  styleUrl: './sanitizer.component.css'
+})
+export class SanitizerComponent {
+
+}
