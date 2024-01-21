2cc9e4e93cd143dd22a8ace3d1b6983ecb53c5fb
Rodrigo Santos <rodrigom376@gmail.com>
2024-01-21 20:52:09 -0300

    routes

--- form-mopdal/src/app/app.routes.ts
+++ form-mopdal/src/app/app.routes.ts
@@ -1,8 +1,11 @@
 import { Routes } from '@angular/router';
 import { FormComponent } from './component/form/form.component';
 import { HomeComponent } from './component/home/home.component';
+import { ContatosComponent } from './component/contatos/contatos.component';
 
 export const routes: Routes = [
     { path: 'form', component: FormComponent },
     { path: 'home', component: HomeComponent },
+    { path: 'contatos', component: ContatosComponent },
+
 ];
--- form-mopdal/src/app/component/nav/nav.component.ts
+++ form-mopdal/src/app/component/nav/nav.component.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { ModalComponent } from '../form/modal/modal.component';
 import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
+import { Router } from '@angular/router';
+
 
 @Component({
   selector: 'app-nav',
@@ -9,9 +11,21 @@ import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
   templateUrl: './nav.component.html',
   styleUrl: './nav.component.scss'
 })
-export class NavComponent {constructor(private ngbModal: NgbModal) {}
+export class NavComponent {
+  constructor(
+  private ngbModal: NgbModal,
+  private router: Router
+  ) {}
 
 openFormModal() {
   this.ngbModal.open(ModalComponent);
+}
+
+navigateToHome() {
+  this.router.navigate(['/home']);
+}
+
+navigateToContatos() {
+  this.router.navigate(['/contatos']);
 }
 }
